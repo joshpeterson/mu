bb16e29c19045ce249c8f49e93af02fca7fc2978
Josh Peterson <petersonjm1@gmail.com>
2022-08-19 11:01:51 +0000

    Allow the Add opcode to work with 32-bit and 64-bit integers

--- mu/Interpreter.cpp
+++ mu/Interpreter.cpp
@@ -1,6 +1,8 @@
 #include <doctest.h>
 
 #include <cassert>
+#include <limits>
+using std::numeric_limits;
 
 #include "Interpreter.hpp"
 #include "ValueStack.hpp"
@@ -13,16 +15,59 @@
 
 void Add() {
   assert(StackSize() >= 2);
-  int32_t left = Pop().i32();
-  int32_t right = Pop().i32();
-  Push(left + right);
+
+  auto left = Pop();
+  auto right = Pop();
+
+  if (left.Type() == ArgumentType::i64 && right.Type() == ArgumentType::i64)
+    Push(left.i64() + right.i64());
+  else if (left.Type() == ArgumentType::i64 &&
+           right.Type() == ArgumentType::i32)
+    Push(left.i64() + right.i32());
+  else if (left.Type() == ArgumentType::i32 &&
+           right.Type() == ArgumentType::i64)
+    Push(left.i32() + right.i64());
+  else
+    Push(left.i32() + right.i32());
 }
 
-TEST_CASE("Verify add opcode behavior") {
+TEST_CASE("Verify add opcode behavior for 32-bit integers") {
   Push(42);
   Push(43);
   Add();
   CHECK(Pop().i32() == 85);
+}
+
+TEST_CASE("Verify add opcode behavior for 64-bit integers") {
+  const int64_t left = numeric_limits<int64_t>::max() - 20;
+  const int64_t right = 15;
+  const int64_t expected = numeric_limits<int64_t>::max() - 5;
+  Push(left);
+  Push(right);
+  Add();
+  CHECK(Pop().i64() == expected);
+}
+
+TEST_CASE(
+    "Verify add opcode behavior for a 64-bit integer and a 32-bit integer") {
+  const int64_t left = numeric_limits<int64_t>::max() - 20;
+  const int32_t right = 15;
+  const int64_t expected = numeric_limits<int64_t>::max() - 5;
+  Push(left);
+  Push(right);
+  Add();
+  CHECK(Pop().i64() == expected);
+}
+
+TEST_CASE(
+    "Verify add opcode behavior for a 32-bit integer and a 64-bit integer") {
+  const int32_t left = 15;
+  const int64_t right = numeric_limits<int64_t>::max() - 20;
+  const int64_t expected = numeric_limits<int64_t>::max() - 5;
+  Push(left);
+  Push(right);
+  Add();
+  CHECK(Pop().i64() == expected);
 }
 
 void Subtract() {
