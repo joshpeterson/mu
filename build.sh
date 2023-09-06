a9788e206c9badfb64bdf9b5acb3833ad06b68aa
Josh Peterson <petersonjm1@gmail.com>
2023-09-06 06:36:00 -0400

    Add nanobench for benchmarking

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build build
 ./build/mu --test
--- mu/Configuration.hpp
+++ mu/Configuration.hpp
@@ -1,4 +1,9 @@
 #include <doctest.h>
 
+#include <nanobench.h>
+
+// This is implemented in main.cpp.
+const char* GetCurrentTestName();
+
 #include "Log.hpp"
 #include "TestUtilities.hpp"
