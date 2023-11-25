e95d0c70ad4224b55c6175ea99c9be09eb957fcf
Josh Peterson <petersonjm1@gmail.com>
2023-11-24 19:39:11 -0500

    Add comments to the Bytecode types and functions

--- mu/Bytecode.cpp
+++ mu/Bytecode.cpp
@@ -10,17 +10,39 @@ using std::unordered_map;
 
 static unordered_map<OpCode, InstructionMetadata> Instructions;
 
+///
+/// @brief Register all instructions that the VM can execute.
+///
+///
 void InitializeInstructions() {
   RegisterInstruction(OpCode::Add, GetAddMetadata());
   RegisterInstruction(OpCode::Subtract, GetSubtractMetadata());
 }
 
+///
+/// @brief Register a specific instruction.
+///
+/// @param[in] opCode The opcode of the instruction to register.
+/// @param[in] metadata The metadata for the instruction to register.
+///
 void RegisterInstruction(OpCode opCode, InstructionMetadata metadata) {
   Instructions[opCode] = metadata;
 }
 
+///
+/// @brief Check if an instruction exists for the given opcode.
+///
+/// @param[in] opCode The opcode to check for.
+/// @return True if an instruction exists for the given opcode, false otherwise.
+///
 bool HasInstruction(OpCode opCode) { return Instructions.contains(opCode); }
 
+///
+/// @brief Find the opcode for the given instruction name.
+///
+/// @param[in] name The name of the instruction to find.
+/// @return The opcode for the given instruction name.
+///
 OpCode GetOpCode(string name) {
   for (auto& [opCode, metadata] : Instructions) {
     if (metadata.name == name) {
@@ -31,10 +53,28 @@ OpCode GetOpCode(string name) {
   return OpCode::Nop;
 }
 
+///
+/// @brief Lookup the instruction for the given opcode and execute it.
+///
+/// @param[in] opCode The opcode of the instruction to execute.
+///
 void ExecuteInstruction(OpCode opCode) { Instructions[opCode].execute(); }
 
+///
+/// @brief Find the name of the instruction for the given opcode.
+///
+/// @param[in] opCode The opcode of the instruction to find the name of.
+/// @return The name of the instruction for the given opcode.
+///
 string GetInstructionName(OpCode opCode) { return Instructions[opCode].name; }
 
+///
+/// @brief Determine if two instructions are equal.
+///
+/// @param[in] left One of the instructions to compare.
+/// @param[in] right One of the instructions to compare.
+/// @return True if the instructions are equal, false otherwise.
+///
 bool operator==(Instruction left, Instruction right) {
   return left.opCode == right.opCode && left.argument == right.argument;
 }
--- mu/Bytecode.hpp
+++ mu/Bytecode.hpp
@@ -9,39 +9,87 @@ using std::string;
 
 #include "Argument.hpp"
 
-// == Bytecode ==
-//
-// The bytecode for μ is an array of instructions.
-//
-// Each instruction has a unique opcode and an argument. The opcode identifes
-// which instruction should be executed. The argument is optional (some
-// instructions don't need an argument) and it ignored is not required.
+/// == Bytecode ==
+///
+/// The bytecode for μ is an array of instructions.
+///
+/// Each instruction has a unique opcode and an argument. The opcode identifes
+/// which instruction should be executed. The argument is optional (some
+/// instructions don't need an argument) and it ignored is not required.
 
+///
+/// @brief The definition of all opcodes that the VM can execute.
+///
 enum class OpCode {
+
+  ///
+  /// The nop opcode is special - it does nothing.
+  ///
   Nop,
 
-  // The push and pop opcodes are special - they implement value stack
-  // behavior.
+  /////////////////////////////////
+  /// The push and pop opcodes are special - they implement value stack
+  /// behavior.
+  /////////////////////////////////
+
+  ///
+  /// Push a value onto the value stack.
+  ///
   Push,
+
+  ///
+  /// Pop a value from the value stack.
+  ///
   Pop,
 
-  // These opcodes represent the actual instructions the VM can execute.
+  /////////////////////////////////
+  /// These opcodes represent the actual instructions the VM can execute.
+  /////////////////////////////////
+
+  ///
+  /// Add to integer or floating point values.
+  ///
   Add,
+
+  ///
+  /// Add to integer or floating point values.
+  ///
   Subtract
 };
 
 ///
-/// struct Instruction - [TODO:description]
+/// @brief An instruction for the VM to execute.
 ///
 struct Instruction {
+  ///
+  /// The opcode for the instruction.
+  ///
   OpCode opCode;
+
+  ///
+  /// The argument of the instruction.
+  ///
   Argument argument;
 };
 
+///
+/// @brief The type of a function that executes an instruction.
+///
+///
 typedef void (*ExecuteInstructionFunc)();
 
+///
+/// @brief The metadata for an instruction.
+///
 struct InstructionMetadata {
+  ///
+  /// The function that executes the instruction.
+  ///
   ExecuteInstructionFunc execute;
+
+  ///
+  /// The name of the instruction.
+  ///
   string name;
 };
 
@@ -55,6 +103,9 @@ string GetInstructionName(OpCode opCode);
 
 bool operator==(Instruction left, Instruction right);
 
+///
+/// @brief Format an opcode as a string, using the fmt library.
+///
 template <> struct fmt::formatter<OpCode> : formatter<string_view> {
   // parse is inherited from formatter<string_view>.
   template <typename FormatContext>
@@ -73,6 +124,12 @@ template <> struct fmt::formatter<OpCode> : formatter<string_view> {
   }
 };
 
+///
+/// @brief Convert an opcode to string for the doctest library.
+///
+/// @param[in] value The opcide to convert to a string.
+/// @return doctest::String The string representation of the opcode.
+///
 inline doctest::String toString(const OpCode& value) {
   return fmt::format("{}", value).c_str();
 }
