4d59a14c3ef82b728fcd3d58b88755d7cf930a2a
Josh Peterson <petersonjm1@gmail.com>
2023-11-24 19:47:42 -0500

    Add comments to the Log functions

--- mu/Log.cpp
+++ mu/Log.cpp
@@ -4,4 +4,10 @@
 
 using fmt::print;
 
+///
+/// @brief Display a message to standard output. The message will be prepended
+/// with text indicating that it is from the mu VM.
+///
+/// @param[in] message The message to output.
+///
 void Log(string_view message) { print("[=== \u03BC ===] {}\n", message); }
--- mu/Log.hpp
+++ mu/Log.hpp
@@ -8,6 +8,17 @@ using fmt::format;
 
 void Log(string_view message);
 
+///
+/// @brief Display a formatted message, using the fmt library to standard
+/// output. The message will be prepended with text indicating that it is from
+/// the mu VM.
+///
+/// @tparam Args Avoid providing template arguments directly, the compiler will
+/// infer them.
+/// @param[in] s The format string to output.
+/// @param[in] args The format arguments, which will be substituted into the
+/// format string.
+///
 template <typename... Args>
 void Log(fmt::format_string<Args...> s, Args&&... args) {
   Log(fmt::format(s, std::forward<Args>(args)...));
